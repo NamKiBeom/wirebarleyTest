1849d04baf5cf5770ea8704ecfe6c116a69348a0
NamKiBeom <nkb5813@naver.com>
2021-01-12 17:42:47 +0900

    [feat] pickerView 연동 및 Outlet 변수 추가

--- WirebarleyTest/WirebarleyTest/View/ViewController.swift
+++ WirebarleyTest/WirebarleyTest/View/ViewController.swift
@@ -10,13 +10,39 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var remittanceCountryLabel: UILabel!
+    @IBOutlet weak var recipientCountryLabel: UILabel!
+    @IBOutlet weak var exchangeRateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var moneyTextField: UITextField!
+    @IBOutlet weak var moneyUnitLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var countryPicker: UIPickerView!
+    
+    private let countrys = ["한국(KRW)", "일본(JPY)", "필리핀(PHP)"]
+    private var sample: Observable<Int>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         
+        countryPicker.delegate = self
     }
-
-
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countrys.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countrys[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+    }
+}
